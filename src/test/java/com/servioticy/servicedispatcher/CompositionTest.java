8abd5409e5b05ca0f27d7766c0b04fa4cd55409e
Álvaro Villalba Navarro <alvaro.villalba@bsc.es>
2014-07-15 17:01:46 +0200

    improved a bit the testing

--- src/test/java/com/servioticy/servicedispatcher/CompositionTest.java
+++ src/test/java/com/servioticy/servicedispatcher/CompositionTest.java
@@ -166,7 +166,8 @@ public class CompositionTest {
                 String newDescriptor;
                 int i = 0;
                 while((newDescriptor = (String) qc.get()) == null){
-                    if(i==50){
+                    if(i==40){
+                        qc.disconnect();
                         Assert.fail("Timeout");
                     }
                     try {
@@ -177,6 +178,7 @@ public class CompositionTest {
                     }
                     i++;
                 }
+                qc.disconnect();
                 Assert.assertTrue("Return value", newDescriptor != null);
 
                 UpdateDescriptor ud = mapper.readValue(newDescriptor, UpdateDescriptor.class);
@@ -324,7 +326,8 @@ public class CompositionTest {
                 String newDescriptor;
                 int i = 0;
                 while ((newDescriptor = (String) qc.get()) == null) {
-                    if (i == 10) {
+                    if (i==40) {
+                        qc.disconnect();
                         Assert.fail("Timeout");
                     }
                     try {
@@ -335,6 +338,7 @@ public class CompositionTest {
                     }
                     i++;
                 }
+                qc.disconnect();
                 Assert.assertTrue("Return value", newDescriptor != null);
 
                 UpdateDescriptor ud = mapper.readValue(newDescriptor, UpdateDescriptor.class);
@@ -463,7 +467,8 @@ public class CompositionTest {
                 String newDescriptor;
                 int i = 0;
                 while((newDescriptor = (String) qc.get()) == null){
-                    if(i==50){
+                    if(i==40){
+                        qc.disconnect();
                         Assert.fail("Timeout");
                     }
                     try {
@@ -474,6 +479,7 @@ public class CompositionTest {
                     }
                     i++;
                 }
+                qc.disconnect();
                 Assert.assertTrue("Return value", newDescriptor != null);
 
                 UpdateDescriptor ud = mapper.readValue(newDescriptor, UpdateDescriptor.class);
@@ -600,7 +606,8 @@ public class CompositionTest {
                 String newDescriptor;
                 int i = 0;
                 while((newDescriptor = (String) qc.get()) == null){
-                    if(i==50){
+                    if(i==40){
+                        qc.disconnect();
                         Assert.fail("Timeout");
                     }
                     try {
@@ -611,6 +618,7 @@ public class CompositionTest {
                     }
                     i++;
                 }
+                qc.disconnect();
                 Assert.assertTrue("Return value", newDescriptor != null);
 
                 UpdateDescriptor ud = mapper.readValue(newDescriptor, UpdateDescriptor.class);
@@ -738,7 +746,8 @@ public class CompositionTest {
                 String newDescriptor;
                 int i = 0;
                 while((newDescriptor = (String) qc.get()) == null){
-                    if(i==50){
+                    if(i==40){
+                        qc.disconnect();
                         Assert.assertTrue("Filtered", true);
                         return;
                     }
@@ -750,6 +759,7 @@ public class CompositionTest {
                     }
                     i++;
                 }
+                qc.disconnect();
                 Assert.fail("Not filtered");
             }
         });
@@ -863,7 +873,8 @@ public class CompositionTest {
                 String newDescriptor;
                 int i = 0;
                 while((newDescriptor = (String) qc.get()) == null){
-                    if(i==50){
+                    if(i==40){
+                        qc.disconnect();
                         Assert.fail("Timeout");
                     }
                     try {
@@ -874,6 +885,7 @@ public class CompositionTest {
                     }
                     i++;
                 }
+                qc.disconnect();
                 Assert.assertTrue("Return value", newDescriptor != null);
 
                 UpdateDescriptor ud = mapper.readValue(newDescriptor, UpdateDescriptor.class);
@@ -885,7 +897,6 @@ public class CompositionTest {
                 SUChannel suCh = ud.getSu().getChannels().get("$");
                 boolean cValue = (Boolean)suCh.getCurrentValue();
                 Assert.assertTrue("New SU current-value", cValue);
-
             }
         });
     }
@@ -999,7 +1010,8 @@ public class CompositionTest {
                 String newDescriptor;
                 int i = 0;
                 while((newDescriptor = (String) qc.get()) == null){
-                    if(i==50){
+                    if(i==40){
+                        qc.disconnect();
                         Assert.fail("Timeout");
                     }
                     try {
@@ -1010,6 +1022,7 @@ public class CompositionTest {
                     }
                     i++;
                 }
+                qc.disconnect();
                 Assert.assertTrue("Return value", newDescriptor != null);
 
                 UpdateDescriptor ud = mapper.readValue(newDescriptor, UpdateDescriptor.class);
@@ -1021,191 +1034,7 @@ public class CompositionTest {
                 SUChannel suCh = ud.getSu().getChannels().get("$");
                 ArrayList<Boolean> cValue = (ArrayList<Boolean>)suCh.getCurrentValue();
                 Assert.assertTrue("New SU current-value", cValue.get(0) && !cValue.get(1) && !cValue.get(2) && cValue.get(3) && cValue.get(4));
-
             }
         });
     }
-//	@Test
-//	public void testExampleTopology() {
-//
-//		try{
-//
-//			String soid = "1234567890";
-//			String origStreamid = "location";
-//
-//			DispatcherContext dc = new DispatcherContext();
-//            dc.loadConf(null);
-//
-//			ObjectMapper mapper = new ObjectMapper();
-//
-//			// Subscriptions document
-//			String subscriptions =	"{'subscriptions': []}";
-//			// Subscriber SO
-//			String so =	"{\n" +
-//                        "    \"streams\": {\n" +
-//                        "        \"weather\": {\n" +
-//                        "            \"channels\":" +
-//                        "                \"temperature\": {\n" +
-//                        "                    \"type\": \"number\",\n" +
-//                        "                    \"unit\": \"degrees\"\n" +
-//                        "                }\n" +
-//                        "            }\n" +
-//                        "        },\n" +
-//                        "        \"fahrenheit\": {\n" +
-//                        "            \"channels\": {\n" +
-//                        "                \"f\": {\n" +
-//                        "                  \"current-value\": \"function(weather){return weather.channels.temperature.current-value * 1.8 + 32}\",\n" +
-//                        "                    \"type\": \"number\"\n" +
-//                        "                }\n" +
-//                        "            }\n" +
-//                        "        },\n" +
-//                        "        \"aboveSeventy\": {\n" +
-//                        "            \"channels\": {\n" +
-//                        "                \"temperature\": {\n" +
-//                        "                    \"current-value\": \"function(fahrenheit){return fahrenheit.channels.f.current-value}\",\n" +
-//                        "                    \"type\": \"number\"\n" +
-//                        "                }\n" +
-//                        "            },\n" +
-//                        "          \"post-filter\": \"function(result){result.channels.temperature.current-value > 70}\"\n" +
-//                        "        }\n" +
-//                        "    }\n" +
-//                        "}";
-//
-//			// group1 SU
-//			String weatherSU=	"{\n" +
-//                                "    \"channels\": {\n" +
-//                                "        \"temperature\":  {\n" +
-//                                "            \"current-value\": 80\n" +
-//                                "\t\t}\n" +
-//                                "    },\n" +
-//                                "    \"lastUpdate\": 1199192639\n" +
-//                                "}";
-//
-//			// near SU
-//			String nearSU =		"{" +
-//									"\"channels\": {" +
-//										"\"n\":{" +
-//											"\"current-value\": true" +
-//										"}" +
-//									"}," +
-//									"\"lastUpdate\": 1392981636" +
-//								"}";
-//			// proximity SU
-//			String proxSU = 	"{" +
-//									"\"channels\": {" +
-//										"\"p\":{" +
-//											"\"current-value\": 0.3234" +
-//										"}" +
-//									"}," +
-//									"\"lastUpdate\": 1392981236" +
-//								"}";
-//
-//			//  Group last update request
-//			SOGroup group = new SOGroup();
-//			group.setStream("location");
-//			ArrayList<String> soids = new ArrayList<String>();
-//			soids.add("origin4");
-//			soids.add("origin5");
-//			group.setSoIds(soids);
-//
-//			// Mocking up the rest calls...
-//			RestClient restClient = mock(RestClient.class, withSettings().serializable());
-//			// get opid
-//			when(restClient.restRequest(
-//					dc.restBaseURL
-//					+ "private/" + opid, null,
-//					RestClient.GET,
-//					null)).thenReturn(new RestResponse("", 200));
-//			// get subscriptions
-//			when(restClient.restRequest(
-//					dc.restBaseURL
-//					+ "private/" + soid + "/streams/weather"
-//					+ "/subscriptions/", null, RestClient.GET,
-//					null)).thenReturn(new RestResponse(subscriptions, 200));
-//            when(restClient.restRequest(
-//                    dc.restBaseURL
-//                            + "private/" + soid + "/streams/fahrenheit"
-//                            + "/subscriptions/", null, RestClient.GET,
-//                    null)).thenReturn(new RestResponse(subscriptions, 200));
-//            when(restClient.restRequest(
-//                    dc.restBaseURL
-//                            + "private/" + soid + "/streams/aboveSeventy"
-//                            + "/subscriptions/", null, RestClient.GET,
-//                    null)).thenReturn(new RestResponse(subscriptions, 200));
-//
-//			// get so
-//			when(restClient.restRequest(
-//					dc.restBaseURL
-//					+ "private/" + soid, null, RestClient.GET,
-//					null)).thenReturn(new RestResponse(so, 200));
-//			// store new SUs
-//			when(restClient.restRequest(
-//					any(String.class),
-//					any(String.class), eq(RestClient.PUT),
-//					any(Map.class))).thenReturn(new RestResponse("", 200));
-//
-//
-//			TopologyBuilder builder = new TopologyBuilder();
-//			QueueClient qc = QueueClient.factory("queue-simple.xml");
-//			qc.connect();
-//
-//
-//			FeederSpout feeder = new FeederSpout(new Fields("opid", "soid", "streamid", "su"));
-//
-//			builder.setSpout("dispatcher", feeder);
-//
-//	        builder.setBolt("checkopid", new CheckOpidBolt(dc, restClient), 1)
-//	        	.shuffleGrouping("dispatcher");
-//	        builder.setBolt("subretriever", new SubscriptionRetrieveBolt(dc, restClient), 1)
-//        		.shuffleGrouping( "checkopid", "subscription");
-//
-//	        builder.setBolt("streamdispatcher", new StreamDispatcherBolt(dc, restClient), 1)
-//	    		.shuffleGrouping("subretriever", "internalSub")
-//	    		.shuffleGrouping("checkopid", "stream");
-//	        builder.setBolt("streamprocessor", new StreamProcessorBolt(dc, qc, restClient), 1)
-//				.fieldsGrouping("streamdispatcher", new Fields("soid", "streamid"));
-//
-//
-//	        Config conf = new Config();
-//	        conf.setDebug(true);
-//
-//	    	LocalCluster cluster = new LocalCluster();
-//	    	cluster.submitTopology("dispatcher", conf, builder.createTopology());
-//
-//	    	feeder.feed(new Values("sometestopid", "1234567890", "weather", weatherSU));
-//
-//	    	String newDescriptor;
-//	    	while((newDescriptor = (String) qc.get()) == null){
-//	    		try {
-//					Thread.sleep(1000);
-//				} catch (InterruptedException e) {
-//					// TODO Auto-generated catch block
-//					e.printStackTrace();
-//				}
-//	    	}
-//	    	Assert.assertTrue("Return value", newDescriptor != null);
-//
-//	    	UpdateDescriptor ud = mapper.readValue(newDescriptor, UpdateDescriptor.class);
-//
-//	    	Assert.assertTrue("Operation id", ud.getOpid() != null);
-//	    	Assert.assertTrue("Origin SO id", ud.getSoid().equals(soid));
-//	    	Assert.assertTrue("Origin stream id", ud.getStreamid().equals("fahrenheit"));
-//	    	Assert.assertTrue("New SU timestamp", ud.getSu().getLastUpdate() == 1199192639);
-//	    	SUChannel such = ud.getSu().getChannels().get("f");
-//	    	double proximity = (Double)such.getCurrentValue();
-//	    	Assert.assertTrue("New SU current-value", proximity == 176);
-//		} catch (RestClientException e) {
-//			fail("Test failed: " + e.getMessage() + "\n" + e.getStackTrace());
-//		} catch (RestClientErrorCodeException e) {
-//			fail("Test failed: " + e.getMessage() + "\n" + e.getStackTrace());
-//		} catch (JsonParseException e) {
-//			fail("Test failed: " + e.getMessage() + "\n" + e.getStackTrace());
-//		} catch (JsonMappingException e) {
-//			fail("Test failed: " + e.getMessage() + "\n" + e.getStackTrace());
-//		} catch (IOException e) {
-//			fail("Test failed: " + e.getMessage() + "\n" + e.getStackTrace());
-//		} catch (QueueClientException e) {
-//			fail("Test failed: " + e.getMessage() + "\n" + e.getStackTrace());
-//		}
-//	}
 }
