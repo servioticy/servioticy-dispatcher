7a1e1ccd949be6b7fad33b7b739a56cb015b7c54
Álvaro Villalba Navarro <alvaro.villalba@bsc.es>
2014-10-16 15:12:59 +0200

    Minor fix

--- unmanaged-dependencies.sh
+++ unmanaged-dependencies.sh
@@ -4,13 +4,13 @@ DIR=$PWD
 PROJECT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 LOCAL_REPO=$PROJECT_DIR/repo
 SOURCES=$PROJECT_DIR/tmp
-GIT_REPOS=( "https://github.com/nopbyte/servioticy-pdp.git" "https://github.com/WolframG/Rhino-Prov-Mod.git" )
-ARTIFACTIDS=( "servioticy-pdp" "rhino" )
-VERSIONS=( "0.1.0" "1.7R4-mod" )
-GROUPIDS=( "de.passau.uni" "org.mozilla" )
-BUILD_CMDS=( "gradle jar"  "ant jar" )
-JAR_FILE=( "build/libs/PDPComponentServioticy-0.1.0.jar" "build/rhino1_7R5pre/js.jar" )
-REVISIONS=( "d379e1d457e0c50e47a9105a89a0b618288c7887" "5fe98c37e9977e6035734b4387b54497c2b35520")
+GIT_REPOS=( "https://github.com/WolframG/Rhino-Prov-Mod.git" )
+ARTIFACTIDS=( "rhino" )
+VERSIONS=( "1.7R4-mod" )
+GROUPIDS=( "org.mozilla" )
+BUILD_CMDS=( "ant jar" )
+JAR_FILE=( "build/rhino1_7R5pre/js.jar" )
+REVISIONS=( "5fe98c37e9977e6035734b4387b54497c2b35520")
 
 echo $PROJECT_DIR
 echo $LOCAL_REPO
